c21e11a462a089a1d0c966640be2e4955c5786ef
Javier Manobanda <51manobandajavier@gmail.com>
2021-10-11 12:03:47 -0500

    organice folder

--- backend/public_jobs/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PublicJobsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'public_jobs'
-    verbose_name = 'Public_Jobs'
